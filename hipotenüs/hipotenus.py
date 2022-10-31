c48c913166dfe1f4fccf73158fd593da7fb99720
Speffa <atcekenemir@gmail.com>
2022-10-31 20:18:00 +0300

    Create hipotenus.py

---
+++ hipotenüs/hipotenus.py
@@ -0,0 +1,7 @@
+#Hipotenüs Formülü a**2  + b**2 = c**2
+
+kenar1 = int(input("1.Kenarı Giriniz : "))
+kenar2 = int(input("2.Kenarı Giriniz : "))
+hipotenüs = ((kenar1**2) + (kenar2**2))**0.5
+hipotenüs = int(hipotenüs)
+print(f"{kenar1}-{kenar2}-{hipotenüs} Üçgeni") 
